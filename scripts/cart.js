a821e09fc757d1f82b798e5ef724cf250b340de5
Deanmark <deansy0718@gmail.com>
2026-01-20 08:45:30 +0800

    added remove item function in cart page

--- scripts/cart.js
+++ scripts/cart.js
@@ -1,14 +1,28 @@
 const cartItems = document.querySelector('.cart-items');
 const totalHtml = document.querySelector('.total');
-
 let cartMain = JSON.parse(localStorage.getItem('getCart')) || [];
+
+renderItem()
+function renderItem() {
+
 let total = 0;
 
-cartMain.forEach( items => {
+cartMain.forEach( (items, i) => {
     total += items.price;
-    cartItems.innerHTML += ` <span>${items.name}</span>
+
+    cartItems.innerHTML += `
+     <span>${items.name}</span>
      <span>$${(items.price / 100).toFixed(2)}</span>
+     <button onclick="cartMain.splice(${i}, 1); renderItem(); removeItem();">Remove</button><br>
     `;
 });
 
-totalHtml.textContent = `Total: $${total}`;
+
+
+totalHtml.textContent = `Total: $${(total).toFixed(2)}`;
+}
+
+function removeItem() {
+    localStorage.removeItem('getCart');
+    cartItems.innerHTML = '';
+}
